63af048f94d563fa32696b8b719adbcaf3c36adb
Jonathan Bastien-Filiatrault <joe@x2a.org>
2025-07-21 20:49:53 -0400

    Make the linux example fully buildable and the rtic example closer to building cleanly.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,15 +1,16 @@
 extern crate bme280;
-extern crate linux_embedded_hal as hal;
 
 use bme280::i2c::BME280;
-use hal::{Delay, I2cdev};
 use std::thread;
 use std::time::Duration;
 
+#[cfg(target_os = "linux")]
 fn main() {
+    use linux_embedded_hal::{Delay, I2cdev};
+
     let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
     let mut bme280 = BME280::new_secondary(i2c_bus);
-    let delay = /*... some delay */
+    let mut delay = Delay;
     bme280.init(&mut delay).unwrap();
     loop {
         let measurements = bme280.measure(&mut delay).unwrap();
@@ -18,4 +19,10 @@ fn main() {
         println!("Pressure = {} pascals", measurements.pressure);
         thread::sleep(Duration::from_secs(1));
     }
+}
+
+#[cfg(not(target_os = "linux"))]
+fn main() {
+    println!("This example is only for Linux with I2C support.");
+    println!("Please run it on a compatible device with the BME280 sensor connected via I2C.");
 }
